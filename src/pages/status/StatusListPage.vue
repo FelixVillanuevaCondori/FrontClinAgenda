a605b2472a9a7beaf7258b6d1c9403d8ee4de209
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-04-01 22:14:22 -0300

    Adicionei Tabelas
    Criação de tabelas

---
+++ src/pages/status/StatusListPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { DefaultTemplate } from '@/template'
+const ItensDaTabela = [
+  {
+    name: 'African Elephant',
+    species: 'Loxodonta africana',
+    diet: 'Herbivore',
+    habitat: 'Savanna, Forests'
+  }
+  // ... more items
+]
+</script>
+
+<template>
+  <DefaultTemplate>
+    <v-data-table :items="ItensDaTabela"></v-data-table>
+  </DefaultTemplate>
+</template>
